53717f732cc12fc6a710dbdfa9d9fff9a55fc603
itskme <160423464+itskme@users.noreply.github.com>
2024-08-15 21:01:04 +0000

    readline modification
    redid hello.js to use readline, installed via npm. The program asks you for your name and you give it, it appends it to "Hello, "

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Please enter your name:");
+rl.on("line", (name) => {
+  const greeting = `Hello, ${name}`;
+  console.log(greeting);
+  rl.close();
+});
